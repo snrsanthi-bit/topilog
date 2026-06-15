49ef47d16d4384f76ae7b70eebd19b46804598e9
Itakura Daisuke <snrsanthi@gmail.com>
2026-06-16 08:39:14 +0900

    Add TopicMessage associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :event
 
+  has_many :topic_messages, dependent: :destroy
+  has_many :topics, through: :topic_messages
+
   validates :speaker_name, presence: true
   validates :content, presence: true
   validates :posted_at, presence: true
---
+++ test/models/topic_message_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TopicMessageTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
