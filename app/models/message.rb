460f15891f3ddada02e9826bf027b3e50879a76b
Itakura Daisuke <snrsanthi@gmail.com>
2026-06-16 22:36:35 +0900

    Add topic-message keyword matching

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,4 +7,11 @@ class Message < ApplicationRecord
   validates :speaker_name, presence: true
   validates :content, presence: true
   validates :posted_at, presence: true
+
+  def match_topics!
+    event.topics.each do |topic|
+      next unless content.include?(topic.name)
+      topic_messages.find_or_create_by!(topic: topic)
+    end
+  end
 end
