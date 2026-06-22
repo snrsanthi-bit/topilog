318d3fa2e367d8ad139aefa830db3bed2e9a5a35
Itakura Daisuke <snrsanthi@gmail.com>
2026-06-22 15:43:39 +0900

    Add user nickname feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :events, dependent: :destroy
+  validates :name, presence: true
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
